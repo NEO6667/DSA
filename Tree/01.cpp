cb049b02de77749d5185020dfbec1f7c0c0263e5
NEO6667 <princeyashc7568@gmail.com>
2025-07-15 09:10:57 +0530

    Tree implementation

---
+++ Tree/01.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* left;
+        node* right;
+
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+node* buildTree(){
+    cout<<"Enter data: "<<endl;
+    int data;
+    cin>>data;
+    
+    if(data == -1){
+        return NULL;
+    }
+    node* root=new node(data);
+
+    cout<<"Enter left: "<<data<<endl;
+    root->left=buildTree();
+
+    cout<<"Enter right: "<<data<<endl;
+    root->right=buildTree();
+
+    return root;
+}
+
+int main(){
+    node* root=buildTree();
+
+    return 0;
+}
