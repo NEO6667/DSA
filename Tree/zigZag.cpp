8a60f12bed6c0067aae25d9734d39551e3f38dfd
NEO6667 <princeyashc7568@gmail.com>
2025-07-16 16:23:47 +0530

    question added

---
+++ Tree/zigZag.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root) return res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool leftToRight = true;
+
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> ans(n);
+            for(int i=0; i<n; i++){
+                TreeNode* temp = q.front();
+                q.pop();
+                int index = leftToRight ? i : n - i -1;;
+                ans[index] = temp->val;
+
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
+            }
+            //change dir
+            leftToRight = !leftToRight;
+            res.push_back(ans);
+
+        }
+        return res;
+        
+    }
+};
